1247286dc8efbd7ca6d29244db088722b1cf5a93
20151074010603 <20151074010603@MC266467.ifrn.local>
2018-09-24 10:25:25 -0300

    Desfazendo

--- BibliotecaJulioMEL/src/biblioteca/comandos/logica/Logica.java
+++
@@ -1,8 +0,0 @@
-package biblioteca.comandos.logica;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Logica {
-	public String execute(HttpServletRequest request, HttpServletResponse response);
-}
