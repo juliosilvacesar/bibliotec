6ff22dadd9b91a47dc9bcd55b82d720b9ee2ca06
20151074010603 <20151074010603@MC266467.ifrn.local>
2018-10-01 11:31:43 -0300

    adicionando o retorno do metodo home da classe de controle.Gloria a Dexs

--- src/agendaspring/controllers/HomeController.java
+++ src/agendaspring/controllers/HomeController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	@RequestMapping("/")
-	public void home()	{
+	public String home(){
 		
 		System.out.println("Chamou o meu controller");
+		return "home.jsp";
 	}
 
 }
