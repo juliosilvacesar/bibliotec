21f9693432b1726d0a0d280fed6acb5ec0f29641
20151074010603 <20151074010603@MC266467.ifrn.local>
2018-10-01 10:50:53 -0300

    criando configuração do servletMApping

--- src/agendaspring/controllers/HomeController.java
+++ src/agendaspring/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package agendaspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	@RequestMapping("/")
 	public void home()	{
+		
 		System.out.println("Chamou o meu controller");
 	}
 
